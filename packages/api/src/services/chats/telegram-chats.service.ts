37d2322068cb8ede81648b186963eeffe1dd7eb8
Tanvir Ahmed <mta.coder97@gmail.com>
2021-12-22 21:57:17 +0000

    updated telegram chats service on api to remove mock chats.

--- packages/api/src/services/chats/telegram-chats.service.ts
+++ packages/api/src/services/chats/telegram-chats.service.ts
@@ -4,7 +4,6 @@ import { Api } from "telegram";
 
 import { AbstractTelegramAuthService } from "../auth/abstract-telegram-auth.service";
 import { AbstractChatsService } from "./abstract-chats.service";
-import mockChats from "./mock-chats.json";
 
 @Injectable()
 export class TelegramChatsService extends AbstractChatsService {
@@ -15,14 +14,13 @@ export class TelegramChatsService extends AbstractChatsService {
   }
 
   public async fetchChats(): Promise<Api.TypeChat[]> {
-    // const client = this.telegramAuthService.getClient();
+    const client = this.telegramAuthService.getClient();
 
-    // const chatResults: Api.messages.TypeChats = await client.invoke(
-    //   new Api.messages.GetAllChats({ exceptIds: [] })
-    // );
+    const chatResults: Api.messages.TypeChats = await client.invoke(
+      new Api.messages.GetAllChats({ exceptIds: [] })
+    );
 
-    // const chats = chatResults.chats;
-    const chats = mockChats as any;
+    const chats = chatResults.chats;
 
     return Promise.resolve(chats);
   }
